c7cb30e6c0ec4c134d208b96b699b0bb9fed3755
xieyang <372282766@qq.com>
2015-09-25 10:03:49 +0800

    user Action层

---
+++ springmvc-learn/src/main/java/com/xieyang/action/UserAction.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Copyright (C) 2015 ShenZhen ComTop Information Technology Co.,Ltd
+ * All Rights Reserved.
+ * 本软件为深圳康拓普开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、
+ * 复制、修改或发布本软件.
+ *****************************************************************************/
+package com.xieyang.action;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * User Action
+ *
+ *
+ * @author  xieyang
+ * @since   1.0
+ * @version 2015-09-25 xieyang
+ */
+@Controller
+public class UserAction {
+
+}
